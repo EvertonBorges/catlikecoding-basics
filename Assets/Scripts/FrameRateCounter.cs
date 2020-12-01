256602b1af1bbf97b42a7d6046be2919cdb9c10b
EvertonBorges <evertonsoaresborges@gmail.com>
2020-12-01 19:44:42 -0400

    Measuring Performance pt-2

---
+++ Assets/Scripts/FrameRateCounter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FrameRateCounter : MonoBehaviour
+{
+    public enum DisplayMode { FPS, MS }
+    
+    [SerializeField]
+	TextMeshProUGUI display = default;
+
+	[SerializeField]
+	DisplayMode displayMode = DisplayMode.FPS;
+
+    [SerializeField, Range(0.1f, 2f)]
+	float sampleDuration = 1f;
+
+    private int frames = 0;
+
+	private float duration = 0f;
+    private float bestDuration = float.MaxValue;
+    private float worstDuration = 0f;
+
+    void Update () 
+    {
+		float frameDuration = Time.unscaledDeltaTime;
+        frames += 1;
+		duration += frameDuration;
+
+        if (frameDuration < bestDuration)
+			bestDuration = frameDuration;
+		if (frameDuration > worstDuration)
+			worstDuration = frameDuration;
+
+		if (duration >= sampleDuration) 
+        {
+			if (displayMode == DisplayMode.FPS)
+				display.SetText("FSP\n{0:1}\n{1:1}\n{2:1}", 1f / bestDuration, frames / duration, 1f / worstDuration);
+			else
+				display.SetText("MS\n{0:1}\n{1:1}\n{2:1}", 1000f * bestDuration, 1000f * duration / frames, 1000f * worstDuration);
+			frames = 0;
+			duration = 0f;
+			bestDuration = float.MaxValue;
+			worstDuration = 0f;
+		}
+	}
+
+}
--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -29,19 +29,15 @@ public class Graph : MonoBehaviour
     FunctionLibrary.FunctionEnum transitionFunction;
 
     void Awake() {
-        points = new Transform[resolution];
-
         float step = 2f / resolution;
-        Vector3 scale = Vector3.one * step;
-        Vector3 position = Vector3.zero;
+        var scale = Vector3.one * step;
+
+        points = new Transform[resolution * resolution];
         for (int i = 0; i < points.Length; i++)
         {
             Transform point = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
-            point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
-
             points[i] = point;
         }
     }
